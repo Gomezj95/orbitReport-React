a0689dd4428bf2a45594cf562fb9652888a4b8b4
Gomezj95 <jonsalgom@gmail.com>
2023-08-14 21:37:32 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import Banner from "./components/Banner";
+import { useState } from "react";
+import satData from "./components/satData";
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) =>
+    data.orbitType))]
+  const fitlerByType = (currentTyoe) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbit === currentType;
+    });
+    setSat(displaySats)
+  };
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons 
+      fitlerByType={fitlerByType}
+      setSat={setSat}
+      displaySats={displaySats}
+      />
+      <Table sat={sat}/>
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,16 +3,25 @@ const Table = ({ sat }) => {
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellites</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {sat.map((data, id) => {
+          return(
+            <tr key ={id}>
+          <td>{data.name}</td>
+          <td>{data.type}</td>
+          <td>{data.launchDate}</td>
+          <td>{data.operational ? "active" : "inactive"}</td>
+            </tr>
+          )}
+         )}
         </tbody>
       </table>
   );
 };
-
 export default Table;
